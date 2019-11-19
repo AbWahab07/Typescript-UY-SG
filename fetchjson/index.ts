1b61f5c9c49275f42b76443f02f0015bfa75af73
Abdul Wahab <abwahab708@gmail.com>
2019-11-19 19:50:15 +0500

    Created a bug by storing the Axios promise response into a variable
    Tried to access the properties which don't even exist but the problem is that we was not able to see/catch the bug until we ran the code.

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -3,5 +3,13 @@ import axios from "axios";
 const url = "http://jsonplaceholder.typicode.com/todos/1";
 
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data;
+
+  const { ID, Title, finished } = todo;
+
+  console.log(`
+    Todo with Id: ${ID}
+    Has a title of: ${Title}
+    Is it finished: ${finished}
+  `);
 });
